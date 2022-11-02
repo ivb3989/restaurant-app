83788af32b1dfdde5fa55f6db8725d8b0630eb77
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-11-02 11:06:30 +0200

    create new project

---
+++ pages/restaurants/[name].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import restaurants from "@/data.json"
+
+const route = useRoute()
+const name = route.params.name
+
+const restaurant = restaurants.find(r => r.name === name)
+</script>
+
+<template>
+    <div>
+        {{restaurant}}
+    </div>
+</template>
