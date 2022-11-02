aa5c7971b9e5d002fe5653fbbf78785e327e872b
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2022-11-02 12:29:17 +0200

    create error layout

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot name="header" />
+        <div>
+            <img src="https://cdn-icons-png.flaticon.com/512/3747/3747004.png" alt="">
+        </div>
+        <slot name="redirectEl" />
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 10rem;
+}
+</style>
